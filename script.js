0015be592f3fe2ce5697758bb6374da68cfaa12c
OpdSteve <opdsteve@gmail.com>
2023-11-08 11:41:48 +0100

    General update

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll('#menu a').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
